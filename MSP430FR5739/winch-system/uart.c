72fed82cf9294c2b43214c805f3af31040d290b1
MoizLokh <mlokhandwala360@gmail.com>
2022-06-18 10:28:09 -0700

    [FIX] - Minor correction on the uart baud rate comment to specify 19200

--- MSP430FR5739/winch-system/uart.c
+++ MSP430FR5739/winch-system/uart.c
@@ -22,7 +22,7 @@ void init_uart(void) {
     UCA1CTLW0 |= UCSWRST + UCSSEL__SMCLK;   // Initially set UART A1 to reset and select SMCLK 1Mhz clk
 
     UCA1BRW = 0x3;
-    UCA1MCTLW |= UCOS16 + 0x0440; // config for baud 38400 with 1Mhz clk. OCOS16 - 1, UCBRF = 0x40 (Lower Byte) and UCBRS = 0x04 (Upper Byte)
+    UCA1MCTLW |= UCOS16 + 0x0440; // config for baud 19200 with 1Mhz clk. OCOS16 - 1, UCBRF = 0x40 (Lower Byte) and UCBRS = 0x04 (Upper Byte)
 
     //-- Port 2, pin 6 USBTX and 5 USBRX
     P2SEL1 |= BIT5 + BIT6;       // Pin 5 and 6 are UCA1 TXD and RXD respectively
